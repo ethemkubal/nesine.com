47fd80e989fda25ec7229992ec729714c346a715
ethemkubal <31169778+ethemkubal@users.noreply.github.com>
2023-07-14 03:18:43 +0300

    support oldbrowser

--- src/Index.tsx
+++ src/Index.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { App } from './App';
 import { SelectedCellsProvider } from './components/SelectedCellsContext';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 const rootNode = document.getElementById('app');
 if(rootNode){
   createRoot(rootNode)
